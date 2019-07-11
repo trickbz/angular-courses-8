ea2623052e254140291e2f68f7660b156f059c21
apetrenko <apetrenko@instartlogic.com>
2019-07-11 15:19:39 +0300

    added tests for service

--- src/app/articles.service.spec.ts
+++ src/app/articles.service.spec.ts
@@ -1,12 +1,64 @@
-import { TestBed } from '@angular/core/testing';
+import {fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import { ArticlesService } from './articles.service';
+import { IArticle } from './iarticle';
 
 describe('ArticlesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let articlesService: ArticlesService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ArticlesService
+      ]
+    });
+
+    articlesService = TestBed.get(ArticlesService);
+  });
 
   it('should be created', () => {
-    const service: ArticlesService = TestBed.get(ArticlesService);
-    expect(service).toBeTruthy();
+    expect(articlesService).toBeTruthy();
   });
+
+  it('removeArticle', fakeAsync(() => {
+    const articleIdToRemove = 1;
+    articlesService.removeArticle(articleIdToRemove);
+    let foundArticle: IArticle;
+    articlesService.articles$.subscribe((data) => {
+      foundArticle = data.find(a => a.id === 1);
+    });
+    tick(2000);
+    expect(foundArticle).toBeUndefined();
+  }));
+
+  it('createArticle', fakeAsync(() => {
+    const newArticle: IArticle = {
+      title: 'New Article',
+      text: 'New Article Text'
+    };
+
+    const expectedArticle: IArticle = {
+      ...newArticle,
+      id: 4
+    };
+
+    articlesService.createArticle$(newArticle)
+      .subscribe(article => {
+        expect(article).toEqual(expectedArticle);
+      });
+
+    articlesService.articles$.subscribe(articles => {
+      const foundArticle = articles.find(a => a.id === expectedArticle.id);
+      expect(foundArticle).toEqual(expectedArticle);
+    });
+    tick(2000);
+  }));
+
+  it('articles$', fakeAsync(() => {
+    const articlesStream = articlesService.articles$;
+    articlesStream.subscribe(articles => {
+      expect(articles instanceof Array).toBeTruthy();
+    });
+    tick(2000);
+  }));
 });
